092c60c9b2fef746c88271c37a69d61f27ddd8e5
Ryan Neufeld <ryan@rkn.io>
2014-07-22 08:07:05 -0500

    Choose license

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # pedestal-micro
 
 A Leiningen template for building micro-services in Pedestal.
-
-## License
-
-Copyright © 2014 Ryan Neufeld
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pedestal-micro/lein-template "0.1.0-SNAPSHOT"
   :description "Template for creating Pedestal micro-services"
   :url "https://github.com/rkneufeld/pedestal-micro"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License (MIT)"
+            :url "https://github.com/rkneufeld/pedestal-micro/blob/master/LICENSE"}
   :eval-in-leiningen true)
