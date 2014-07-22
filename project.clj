5ea3d7275d9941417fd667937263a0d40dcf9ac8
Ryan Neufeld <ryan@rkn.io>
2014-07-22 08:22:56 -0500

    Start 0.1.1 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pedestal-micro/lein-template "0.1.0"
+(defproject pedestal-micro/lein-template "0.1.1-SNAPSHOT"
   :description "Template for creating Pedestal micro-services"
   :url "https://github.com/rkneufeld/pedestal-micro"
   :license {:name "The MIT License (MIT)"
