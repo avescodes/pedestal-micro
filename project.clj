47aa9787915f1a312f35e7a8edfc3f21558fc64b
Ryan Neufeld <ryan@rkn.io>
2015-02-05 08:06:17 -0600

    0.3.1: Fix double-main

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pedestal-micro/lein-template "0.3.0"
+(defproject pedestal-micro/lein-template "0.3.1"
   :description "Template for creating Pedestal micro-services"
   :url "https://github.com/rkneufeld/pedestal-micro"
   :license {:name "The MIT License (MIT)"
